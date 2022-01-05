0462339f8c75de8151d86a17ddfe5319a14f45d4
Seth Mulia <smulia@r2hstudent.com>
2022-01-04 19:00:02 -0500

    added java code'

---
+++ script.js
@@ -0,0 +1,32 @@
+var personName = "Seth"
+console.log(personName);
+
+
+var num1 = 3;
+
+var num2 = 5;
+
+var stringNum1 = "3";
+
+var stringNum2 = "5";
+
+var parsNum1 = parseInt(stringNum1);
+var parsNum2 = parseInt(stringNum2);
+
+
+console.log("This is math: " + parseInt(num1 + num2));
+console.log("This is madness: " + (parsNum1 + parsNum2));
+
+var isTrue = true;
+
+console.log(typeof(isTrue));
+
+
+var num3 = 8;
+
+var num4 = 6;
+
+var num5 = num3 - num4;
+
+console.log("Quick maths 8 minus 6 is " + (num3 - num4));
+console.log("This is a variable" +)
